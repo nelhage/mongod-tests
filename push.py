4d3f35791fe73df2d0d42861479aeb23ff50d28e
Nelson Elhage <nelhage@nelhage.com>
2013-08-21 10:45:24 -0700

    push: Add --count as an option.

--- push.py
+++ push.py
@@ -8,10 +8,13 @@ import pymongo
 import mongo
 
 class PushTest(object):
-    COUNT = 5000
-
     def parse_args(self, args):
         parser = optparse.OptionParser("Usage: %prog [args]")
+        parser.add_option('--count',
+                          help='How many $push ops to apply',
+                          action='store',
+                          default=10000,
+                          type='int')
         mongo.add_mongod_options(parser)
         (self.options, args) = parser.parse_args(args)
 
@@ -24,18 +27,18 @@ class PushTest(object):
     def test_push(self):
         client = self.mongo.connect_primary()
 
-        logging.info("$push'ing %d items, one at a time.", self.COUNT)
+        logging.info("$push'ing %d items, one at a time.", self.options.count)
         collection = client['test']['test_push']
         collection.drop()
         _id = collection.insert({"nums": []})
-        for i in xrange(self.COUNT):
+        for i in xrange(self.options.count):
             collection.update({'_id': _id}, { '$push': { 'nums': i }})
 
         start = time.time()
         status = client['admin'].command("replSetGetStatus")
 
         start_optime = [m for m in status['members'] if m['state'] == 1][0]['optimeDate']
-        logging.info("Committed %d $push's, optime=%s...", self.COUNT, start_optime)
+        logging.info("Committed %d $push's, optime=%s...", self.options.count, start_optime)
 
         logging.info("Waiting on secondaries...")
         while len([m for m in status['members'] if m['optimeDate'] < start_optime]):
